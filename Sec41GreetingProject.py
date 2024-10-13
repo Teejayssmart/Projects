36c357267aadd5f75b08f037eedf5f39de40b8fa
Teejayssmart <teejay.adedosu@toshh.online>
2024-10-13 14:00:15 +0100

    test ammend

--- Sec41GreetingProject.py
+++ Sec41GreetingProject.py
@@ -1,2 +1,2 @@
-name = input("What is your name: ")
-print(f"Welcome {name}")
+name1 = input("What is your name: ")
+print(f"Welcome {name1}")
--- Sec4Proj3GrossPay.py
+++ Sec4Proj3GrossPay.py
@@ -4,6 +4,6 @@ userHours = input("Please enter number of hours worked: \n")
 ratePerHour = input("Please enter rate per hour: \n")
 hours = float(userHours)
 rate = float(ratePerHour)
-grossPay1 = round(hours * rate,2)
-print(f"Your gross pay is {grossPay1}")
+grossPay = round(hours * rate,2)
+print(f"Your gross pay is {grossPay}")
 
