6edde822b539f636a7747ac4cef2dad8200eaf78
Teejayssmart <teejay.adedosu@toshh.online>
2024-10-23 19:05:57 +0100

    update newMessage

--- Sec4Proj2.py
+++ Sec4Proj2.py
@@ -1,6 +1,6 @@
 print("Welcome to the Group Name Generator.")
-print("What are your favourite colours? ")
+print("What are your favourite colours in all? ")
 colour = input()
-print("What is your favourite animal or animals?")
+print("What is your favourite animal or animals out of all?")
 group = input()
 print(f"Your group name could be {colour} {group}s")
