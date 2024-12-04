9c75ecd4a49d68fd50e69048c045e922568b8a84
Teejay Adedosu <teejay.adedosu@toshh.online>
2024-12-04 02:31:28 +0000

    Delete sec3Proj3.py

--- sec3Proj3.py
+++
@@ -1,8 +0,0 @@
-# Write a program that uses input to prompt a user
-# for their name and then prints to the
-# console the number of characters in the inputed name.
-#use len to find the lenght of string
-
-name = input("What is your name: ")
-print(len(name))
-
