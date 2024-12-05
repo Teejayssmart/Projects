f7dc8725cae2d34645117ff9b8a20902514f9109
Teejayssmart <teejay.adedosu@toshh.online>
2024-12-05 01:47:20 +0000

    hey

---
+++ Grosspay_with_Overtime_using_Function_Project.py
@@ -0,0 +1,30 @@
+def compute_pay(p_hour, p_rate):
+    if p_hour < 40:
+        pay = round(p_rate * p_hour, 2)
+    else:
+        overtime_rate = 1.5
+
+        overtime = p_hour - 40
+        extra_pay = overtime_rate * overtime * p_rate
+
+        pay = (40 * p_rate) + extra_pay
+    return pay
+
+
+def check_for_float(p_value):
+    try:
+        p_val = float(p_value)
+        return p_val
+    except ValueError:
+        print("There is an error")
+        quit()
+
+
+hours = input("Enter Hours: ")
+hour = check_for_float(hours)
+rate = input("Enter rate: ")
+rate = check_for_float(rate)
+
+
+result = compute_pay(hour, rate)
+print(f"Pay: {result}")
---
+++ Leap_Year_with_Function_Project.py
@@ -0,0 +1,19 @@
+# define a function
+year = int(input("Enter year: "))
+
+
+def leap_year_detector():
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return " This is leap year"
+            else:
+                return "This is not a leap year"
+        else:
+            return "This is leap year"
+
+    else:
+        return "not a leap year"
+
+
+print(leap_year_detector())
