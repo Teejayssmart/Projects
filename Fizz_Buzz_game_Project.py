265a1799abf634456d540b74054038def6e3a1c9
Teejayssmart <teejay.adedosu@toshh.online>
2024-12-15 01:46:55 +0000

    fizz_buzz_game_Project

---
+++ Fizz_Buzz_game_Project.py
@@ -0,0 +1,21 @@
+for num in range(1, 101):
+
+    if num % 5 == 0 and num % 7 == 0:
+        print("FizzBuzz")
+
+    elif num % 5 == 0:
+
+        print("Fizz")
+
+
+    elif num % 7 == 0:
+
+        print("Buzz")
+
+
+
+    else:
+        print(num)
+
+# result = fizz_Buzz_game()
+# print(result)
