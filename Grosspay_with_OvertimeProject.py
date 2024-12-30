4b46d173c62ad66e487148e97be35249ff6ce1af
Teejay Adedosu <teejay.adedosu@toshh.online>
2024-12-30 03:27:01 +0000

    how to edit list using range and len

--- Grosspay_with_OvertimeProject.py
+++ Grosspay_with_OvertimeProject.py
@@ -1,22 +1,16 @@
 hours = int(input("Enter Hours: "))
 rate = int(input("Enter rate: "))
 
-pay = 0
-new_pay = 0
-overtime_rate = 1.5
-extra_hours = 0
-
-if hours == 40:
-    pay = rate * hours
+if hours < 40:
+    pay = round(rate * hours, 2)
     print(f"Pay is {pay}")
 
-elif hours > 40:
-       extra_hours = hours - 40
-       extra_pay_hour = overtime_rate * extra_hours
-       bonus =  extra_pay_hour * extra_hours
-       actual_pay = 40 * rate
-       pay = round(actual_pay + bonus, 2)
-       print(f"Pay is {pay}")
+else:
+    overtime_rate = 1.5
 
+    overtime = hours - 40
+    extra_pay = overtime_rate * overtime * rate
 
+    pay = (40 * rate) + extra_pay
 
+    print(f"Pay is {pay}")
---
+++ manipulating_List.py
@@ -0,0 +1,12 @@
+import math
+
+
+def square_list(p_list):
+    for i in range(len(p_list)):
+        p_list[i] = p_list[i] ** p_list[i]
+    return p_list
+
+
+result = square_list([1, 2, 3])
+
+print(result)
